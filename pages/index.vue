ed2dc21368fd84a5806ec2a6c29f066a9b11566c
Jorge Castellon Jr <jorge@castellon.dev>
2023-04-29 00:28:01 -0700

    Updates a little bit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-3xl">Components</h1>
+    <div class="section">
+      <h2 class="text-2xl">BaseAutocomplete</h2>
+      <BaseAutocomplete />
+    </div>
+    <div class="section">
+      <h2 class="text-2xl">BaseButton</h2>
+      <p>Props: type, color, rounded, variant, block, to, href, size, shadow</p>
+      <BaseButton>Base Button</BaseButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.section {
+  margin-top: 2rem;
+}
+</style>
