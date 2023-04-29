389b77fae6795bc72663e5ec7f54732665911fe4
Jorge Castellon Jr <jorge@castellon.dev>
2023-04-29 14:46:13 -0700

    Button is now NuxtLink

--- components/Base/Button/Button.vue
+++ components/Base/Button/Button.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import type { PropType } from 'vue'
-import { computed } from 'vue'
-import { tv } from 'tailwind-variants'
+import type { PropType } from "vue";
+import { computed } from "vue";
+import { tv } from "tailwind-variants";
 
 const props = defineProps({
   type: {
@@ -9,17 +9,25 @@ const props = defineProps({
     default: undefined,
   },
   color: {
-    type: String as PropType<'primary' | 'secondary' | 'warning' | 'error' | 'success' | 'light' | 'dark'>,
-    default: 'light',
+    type: String as PropType<
+      | "primary"
+      | "secondary"
+      | "warning"
+      | "error"
+      | "success"
+      | "light"
+      | "dark"
+    >,
+    default: "light",
   },
   rounded: {
     type: Boolean,
     default: false,
   },
   variant: {
-    type: String as PropType<'primary' | 'tertiary' | 'ghost'>,
-    default: 'primary',
-    validator: (v: string) => ['primary', 'tertiary', 'ghost'].includes(v),
+    type: String as PropType<"primary" | "tertiary" | "ghost">,
+    default: "primary",
+    validator: (v: string) => ["primary", "tertiary", "ghost"].includes(v),
   },
   block: {
     type: Boolean,
@@ -34,18 +42,21 @@ const props = defineProps({
     default: undefined,
   },
   size: {
-    type: String as PropType<'sm' | 'md' | 'lg'>,
-    default: 'md',
-    validator: (v: string) => ['sm', 'md', 'lg'].includes(v),
+    type: String as PropType<"sm" | "md" | "lg">,
+    default: "md",
+    validator: (v: string) => ["sm", "md", "lg"].includes(v),
   },
   shadow: {
-    type: [Boolean, String] as PropType<boolean | 'sm' | 'md' | 'lg' | 'xl'>,
+    type: [Boolean, String] as PropType<boolean | "sm" | "md" | "lg" | "xl">,
     default: false,
-    validator: (v: boolean | string) => [true, false, 'sm', 'md', 'lg', 'xl'].includes(v),
+    validator: (v: boolean | string) =>
+      [true, false, "sm", "md", "lg", "xl"].includes(v),
   },
-})
+});
 
-const tag = computed(() => (props.to ? 'nuxt-link' : props.href ? 'a' : 'button'))
+const tag = computed(() =>
+  props.to ? "nuxt-link" : props.href ? "a" : "button"
+);
 
 const btn = tv({
   base: `inline-flex items-center gap-2 justify-center border border-transparent text-center font-medium
@@ -53,138 +64,169 @@ const btn = tv({
     transition duration-200 ease-in-out`,
   variants: {
     color: {
-      light: 'bg-white text-gray-800 border-gray-400 hover:bg-gray-100 focus:ring-indigo-600',
-      dark: 'text-white bg-neutral-800 border-neutral-800 focus:ring-neutral-800',
-      primary: 'bg-indigo-600 border-indigo-600 text-white hover:bg-indigo-700 focus:ring-indigo-600',
-      secondary: 'bg-gray-600 border-gray-600 text-white hover:bg-gray-700 focus:ring-gray-600',
-      warning: 'bg-amber-500 border-amber-500 text-white hover:bg-amber-600 focus:ring-amber-500',
-      error: 'bg-rose-600 border-rose-600 text-white hover:bg-rose-700 focus:ring-rose-600',
-      success: 'bg-green-600 border-green-600 text-white hover:bg-green-700 focus:ring-green-600',
+      light:
+        "bg-white text-gray-800 border-gray-400 hover:bg-gray-100 focus:ring-indigo-600",
+      dark: "text-white bg-neutral-800 border-neutral-800 focus:ring-neutral-800",
+      primary:
+        "bg-indigo-600 border-indigo-600 text-white hover:bg-indigo-700 focus:ring-indigo-600",
+      secondary:
+        "bg-gray-600 border-gray-600 text-white hover:bg-gray-700 focus:ring-gray-600",
+      warning:
+        "bg-amber-500 border-amber-500 text-white hover:bg-amber-600 focus:ring-amber-500",
+      error:
+        "bg-rose-600 border-rose-600 text-white hover:bg-rose-700 focus:ring-rose-600",
+      success:
+        "bg-green-600 border-green-600 text-white hover:bg-green-700 focus:ring-green-600",
     },
     size: {
-      sm: 'px-2 py-1 text-sm h-[28px]',
-      md: 'px-3.5 py-1.5 text-sm',
-      lg: 'py-3 px-8 text-lg h-[48px]',
+      sm: "px-2 py-1 text-sm h-[28px]",
+      md: "px-3.5 py-1.5 text-sm",
+      lg: "py-3 px-8 text-lg h-[48px]",
     },
     rounded: {
-      true: 'rounded-full',
-      false: 'rounded-md',
+      true: "rounded-full",
+      false: "rounded-md",
     },
     variant: {
-      primary: '',
-      tertiary: 'bg-transparent',
-      ghost: 'bg-transparent border-none',
+      primary: "",
+      tertiary: "bg-transparent",
+      ghost: "bg-transparent border-none",
     },
     shadow: {
-      true: 'shadow',
-      false: '',
-      sm: 'shadow-sm',
-      md: 'shadow-md',
-      lg: 'shadow-lg',
-      xl: 'shadow-xl',
+      true: "shadow",
+      false: "",
+      sm: "shadow-sm",
+      md: "shadow-md",
+      lg: "shadow-lg",
+      xl: "shadow-xl",
     },
     block: {
-      true: 'w-full',
-      false: '',
+      true: "w-full",
+      false: "",
     },
   },
   defaultVariants: {
-    size: 'md',
-    color: 'light',
+    size: "md",
+    color: "light",
     rounded: false,
   },
   compoundVariants: [
     // primary
     {
-      variant: 'tertiary',
-      color: 'primary',
-      class: 'text-indigo-600 hover:text-white',
+      variant: "tertiary",
+      color: "primary",
+      class: "text-indigo-600 hover:text-white",
     },
     {
-      variant: 'ghost',
-      color: 'primary',
-      class: 'text-indigo-600 hover:bg-indigo-100',
+      variant: "ghost",
+      color: "primary",
+      class: "text-indigo-600 hover:bg-indigo-100",
     },
     // secondary
     {
-      variant: 'tertiary',
-      color: 'secondary',
-      class: 'text-gray-600 hover:text-white',
+      variant: "tertiary",
+      color: "secondary",
+      class: "text-gray-600 hover:text-white",
     },
     {
-      variant: 'ghost',
-      color: 'secondary',
-      class: 'text-gray-600 hover:bg-gray-100',
+      variant: "ghost",
+      color: "secondary",
+      class: "text-gray-600 hover:bg-gray-100",
     },
     // warning
     {
-      variant: 'tertiary',
-      color: 'warning',
-      class: 'text-amber-600 hover:text-white',
+      variant: "tertiary",
+      color: "warning",
+      class: "text-amber-600 hover:text-white",
     },
     {
-      variant: 'ghost',
-      color: 'warning',
-      class: 'text-amber-600 hover:bg-amber-100',
+      variant: "ghost",
+      color: "warning",
+      class: "text-amber-600 hover:bg-amber-100",
     },
     // error
     {
-      variant: 'tertiary',
-      color: 'error',
-      class: 'text-rose-600 hover:text-white',
+      variant: "tertiary",
+      color: "error",
+      class: "text-rose-600 hover:text-white",
     },
     {
-      variant: 'ghost',
-      color: 'error',
-      class: 'text-rose-600 hover:bg-rose-100',
+      variant: "ghost",
+      color: "error",
+      class: "text-rose-600 hover:bg-rose-100",
     },
     // success
     {
-      variant: 'tertiary',
-      color: 'success',
-      class: 'text-green-600 hover:text-white',
+      variant: "tertiary",
+      color: "success",
+      class: "text-green-600 hover:text-white",
     },
     {
-      variant: 'ghost',
-      color: 'success',
-      class: 'text-green-600 hover:bg-green-100',
+      variant: "ghost",
+      color: "success",
+      class: "text-green-600 hover:bg-green-100",
     },
     // light
     {
-      variant: 'tertiary',
-      color: 'light',
-      class: 'text-gray-800 hover:bg-gray-800 hover:text-white',
+      variant: "tertiary",
+      color: "light",
+      class: "text-gray-800 hover:bg-gray-800 hover:text-white",
     },
     {
-      variant: 'ghost',
-      color: 'light',
-      class: 'text-gray-800 hover:bg-gray-100',
+      variant: "ghost",
+      color: "light",
+      class: "text-gray-800 hover:bg-gray-100",
     },
     // dark
     {
-      variant: 'tertiary',
-      color: 'dark',
-      class: 'text-neutral-800 hover:bg-neutral-800 hover:text-white',
+      variant: "tertiary",
+      color: "dark",
+      class: "text-neutral-800 hover:bg-neutral-800 hover:text-white",
     },
     {
-      variant: 'ghost',
-      color: 'dark',
-      class: 'text-neutral-800 hover:bg-neutral-100',
+      variant: "ghost",
+      color: "dark",
+      class: "text-neutral-800 hover:bg-neutral-100",
     },
   ],
-})
+});
 </script>
 
 <template>
+  <NuxtLink
+    v-if="tag == 'nuxt-link'"
+    :to="to"
+    :href="href"
+    :type="type"
+    :class="
+      btn({
+        size,
+        color,
+        rounded,
+        variant,
+        shadow,
+        block,
+      })
+    "
+  >
+    <slot />
+  </NuxtLink>
   <component
-    :is="tag" :to="to" :href="href" :type="type" :class="btn({
-      size,
-      color,
-      rounded,
-      variant,
-      shadow,
-      block,
-    })"
+    v-else
+    :is="tag"
+    :to="to"
+    :href="href"
+    :type="type"
+    :class="
+      btn({
+        size,
+        color,
+        rounded,
+        variant,
+        shadow,
+        block,
+      })
+    "
   >
     <slot />
   </component>
